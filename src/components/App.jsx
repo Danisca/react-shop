fb575abcd48575005b6ea0ce9fe89f0efa3829d1
daniloll <dcanadablandon@gmail.com>
2022-05-17 09:48:12 -0600

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,8 @@
+import {React} from'react';
+import ReactDom from 'react-dom';
+
+const App = ()=>{
+    return <h1>Hellow from react</h1>
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import {React} from 'react';
+import ReactDom from 'react-dom/client';
+import App from './components/App';
+
+const mainContainer = ReactDom.createRoot(document.getElementById('app'));
+mainContainer.render(<App/>);
