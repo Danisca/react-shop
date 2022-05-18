2b28ef0e53f01b73e29386c5008a9d84352738ad
daniloll <dcanadablandon@gmail.com>
2022-05-18 09:27:57 -0600

    add send-email.scss, update import after refactoring app.jsx to routes/

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom/client';
-import App from './components/App';
+import App from './routes/App.jsx';
 
 const mainContainer = ReactDom.createRoot(document.getElementById('app'));
 mainContainer.render(<App/>);
